e3d2155cdd5602a4446890444d98ea92c3b232c9
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-09-16 15:47:43 -0400

    Update dbConfig.java

--- src/main/java/com/co/services/sample/config/dbConfig.java
+++ src/main/java/com/co/services/sample/config/dbConfig.java
@@ -1,55 +1,55 @@
-package com.co.services.sample;
+// package com.co.services.sample;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.*;
-import org.springframework.jdbc.core.JdbcTemplate;
+// import com.zaxxer.hikari.HikariConfig;
+// import com.zaxxer.hikari.HikariDataSource;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.context.annotation.*;
+// import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.sql.DataSource;
+// import javax.sql.DataSource;
 
-@Configuration
-public class Config {
+// @Configuration
+// public class Config {
 
-    private final Logger log = LoggerFactory.getLogger(Config.class);
+//     private final Logger log = LoggerFactory.getLogger(Config.class);
 
-    String serverName = "donssqlv1.database.usgovcloudapi.net";
-    String port = "1433";
-    String databaseName = "MCSSQL-POCV1";
-    String userName = "donssqllogin";
-    String password = "dlykins0A";
+//     String serverName = "donssqlv1.database.usgovcloudapi.net";
+//     String port = "1433";
+//     String databaseName = "MCSSQL-POCV1";
+//     String userName = "donssqllogin";
+//     String password = "dlykins0A";
 
-    // MS SQL Datasource
-    @Primary
-    @Bean("mainDataSource")
-    public DataSource mainDataSource() {
-        log.info("********************************* Creating mainDataSource");
-        HikariConfig config = new HikariConfig();
+//     // MS SQL Datasource
+//     @Primary
+//     @Bean("mainDataSource")
+//     public DataSource mainDataSource() {
+//         log.info("********************************* Creating mainDataSource");
+//         HikariConfig config = new HikariConfig();
 
-        config.setDataSourceClassName("com.microsoft.sqlserver.jdbc.SQLServerDataSource");
-        String url = "jdbc:microsoft:sqlserver://" + serverName + ":" + port + "?useServerPrepStmts=false&rewriteBatchedStatements=true&jdbcCompliantTruncation=false";
-        config.setJdbcUrl(url);
-        config.addDataSourceProperty("serverName", serverName);
-        config.addDataSourceProperty("portNumber", port);
-        config.addDataSourceProperty("databaseName", databaseName);
+//         config.setDataSourceClassName("com.microsoft.sqlserver.jdbc.SQLServerDataSource");
+//         String url = "jdbc:microsoft:sqlserver://" + serverName + ":" + port + "?useServerPrepStmts=false&rewriteBatchedStatements=true&jdbcCompliantTruncation=false";
+//         config.setJdbcUrl(url);
+//         config.addDataSourceProperty("serverName", serverName);
+//         config.addDataSourceProperty("portNumber", port);
+//         config.addDataSourceProperty("databaseName", databaseName);
 
-        config.setUsername(userName);
-        config.setPassword(password);
-        config.setMaximumPoolSize(30);
+//         config.setUsername(userName);
+//         config.setPassword(password);
+//         config.setMaximumPoolSize(30);
 
-        // Default is 60 seconds
-        config.setConnectionTimeout(60*1000);
+//         // Default is 60 seconds
+//         config.setConnectionTimeout(60*1000);
 
-        HikariDataSource ds = new HikariDataSource(config);
+//         HikariDataSource ds = new HikariDataSource(config);
 
-        return ds;
-    }
+//         return ds;
+//     }
 
-    @Bean("mainJdbcTemplate")
-    public JdbcTemplate mainJdbcTemplate() {
-        return new JdbcTemplate(mainDataSource());
-    }
-}
+//     @Bean("mainJdbcTemplate")
+//     public JdbcTemplate mainJdbcTemplate() {
+//         return new JdbcTemplate(mainDataSource());
+//     }
+// }
 
 
