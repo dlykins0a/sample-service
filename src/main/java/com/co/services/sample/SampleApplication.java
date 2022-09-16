c8c78ff56987b284909a9b6e1a9ba851d6dd6d5e
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-09-16 17:45:31 -0400

    Update SampleApplication.java

--- src/main/java/com/co/services/sample/SampleApplication.java
+++ src/main/java/com/co/services/sample/SampleApplication.java
@@ -2,8 +2,9 @@ package com.co.services.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SampleApplication {
 
 	public static void main(String[] args) {
