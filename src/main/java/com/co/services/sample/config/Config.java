940a040a6520fd50e3335489e99abd19a26ce408
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-09-16 20:50:13 -0400

    Update Config.java

--- src/main/java/com/co/services/sample/config/Config.java
+++ src/main/java/com/co/services/sample/config/Config.java
@@ -19,18 +19,17 @@ public class Config {
   
     private final Logger log = LoggerFactory.getLogger(Config.class);
 
-    String serverName = environment.getProperty("environment.servername")
-    String port = environment.getProperty("environment.dbport")
-    String databaseName = environment.getProperty("environment.dbname")
-    String userName = environment.getProperty("environment.username")
-    String password = environment.getProperty("environment.password")
+    String serverName = environment.getProperty("environment.servername");
+    String port = environment.getProperty("environment.dbport");
+    String databaseName = environment.getProperty("environment.dbname");
+    String userName = environment.getProperty("environment.username");
+    String password = environment.getProperty("environment.password");
 
     // MS SQL Datasource
     @Primary
     @Bean("mainDataSource")
     public DataSource mainDataSource() {
         String envSample = environment.getProperty("environment.username");
-        System.out.println("logging env var from yml" + envSample);
         log.info("********************************* logging env var from yml");
         log.info(envSample);
         log.info("********************************* Creating mainDataSource");
