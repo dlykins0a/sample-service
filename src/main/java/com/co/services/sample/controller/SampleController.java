477a33c1b9ebaa990479f751922e336a983320a6
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-09-19 09:11:06 -0400

    Update SampleController.java

--- src/main/java/com/co/services/sample/controller/SampleController.java
+++ src/main/java/com/co/services/sample/controller/SampleController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.core.env.Environment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class SampleController {
