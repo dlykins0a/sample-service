49210d8a95a03abda1f92650c72161575f24ba62
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-09-19 09:59:57 -0400

    Update Config.java

--- src/main/java/com/co/services/sample/config/Config.java
+++ src/main/java/com/co/services/sample/config/Config.java
@@ -1,63 +1,65 @@
-// package com.co.services.sample.config;
+package com.co.services.sample.config;
 
-// import com.zaxxer.hikari.HikariConfig;
-// import com.zaxxer.hikari.HikariDataSource;
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
-// import org.springframework.context.annotation.*;
-// import org.springframework.jdbc.core.JdbcTemplate;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.core.env.Environment;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-// import javax.sql.DataSource;
+import javax.sql.DataSource;
 
-// @Configuration
-// public class Config {
+@Configuration
+public class Config {
 
-//     @Autowired
-//     private Environment environment;
-  
-//     private final Logger log = LoggerFactory.getLogger(Config.class);
+    private final Logger log = LoggerFactory.getLogger(Config.class);
 
-//     String serverName = environment.getProperty("environment.servername");
-//     String port = environment.getProperty("environment.dbport");
-//     String databaseName = environment.getProperty("environment.dbname");
-//     String userName = environment.getProperty("environment.username");
-//     String password = environment.getProperty("environment.password");
+    /*
+     String serverName = environment.getProperty("environment.servername");
+     String port = environment.getProperty("environment.dbport");
+     String databaseName = environment.getProperty("environment.dbname");
+     String userName = environment.getProperty("environment.username");
+     String password = environment.getProperty("environment.password");
+     */
 
-//     // MS SQL Datasource
-//     @Primary
-//     @Bean("mainDataSource")
-//     public DataSource mainDataSource() {
-//         String envSample = environment.getProperty("environment.username");
-//         log.info("********************************* logging env var from yml");
-//         log.info(envSample);
-//         log.info("********************************* Creating mainDataSource");
-//         HikariConfig config = new HikariConfig();
+    String serverName = "azuresql-server-566252580.database.usgovcloudapi.net";
+    String port = "1433";
+    String databaseName = "NEXT_GEN_DB";
+    String userName = "azureuser";
+    String password = "Password!";
 
-//         config.setDataSourceClassName("com.microsoft.sqlserver.jdbc.SQLServerDataSource");
-//         String url = "jdbc:microsoft:sqlserver://" + serverName + ":" + port + "?useServerPrepStmts=false&rewriteBatchedStatements=true&jdbcCompliantTruncation=false";
-//         config.setJdbcUrl(url);
-//         config.addDataSourceProperty("serverName", serverName);
-//         config.addDataSourceProperty("portNumber", port);
-//         config.addDataSourceProperty("databaseName", databaseName);
+    // MS SQL Datasource
+    @Primary
+    @Bean("mainDataSource")
+    public DataSource mainDataSource() {
+        log.info("********************************* Creating mainDataSource");
+        HikariConfig config = new HikariConfig();
 
-//         config.setUsername(userName);
-//         config.setPassword(password);
-//         config.setMaximumPoolSize(30);
+        config.setDataSourceClassName("com.microsoft.sqlserver.jdbc.SQLServerDataSource");
+        String url = "jdbc:microsoft:sqlserver://" + serverName + ":" + port + "?useServerPrepStmts=false&rewriteBatchedStatements=true&jdbcCompliantTruncation=false";
+        config.setJdbcUrl(url);
+        config.addDataSourceProperty("serverName", serverName);
+        config.addDataSourceProperty("portNumber", port);
+        config.addDataSourceProperty("databaseName", databaseName);
 
-//         // Default is 60 seconds
-//         config.setConnectionTimeout(60*1000);
+        config.setUsername(userName);
+        config.setPassword(password);
+        config.setMaximumPoolSize(30);
 
-//         HikariDataSource ds = new HikariDataSource(config);
+        // Default is 60 seconds
+        config.setConnectionTimeout(60*1000);
 
-//         return ds;
-//     }
+        try {
+            HikariDataSource ds = new HikariDataSource(config);
+            return ds;
+        } catch (Exception ex) {
+            log.info("********************************* Create mainDataSource FAILED {}", ex);
+            return new HikariDataSource();
+        }
+    }
 
-//     @Bean("mainJdbcTemplate")
-//     public JdbcTemplate mainJdbcTemplate() {
-//         return new JdbcTemplate(mainDataSource());
-//     }
-// }
-
-
+    @Bean("mainJdbcTemplate")
+    public JdbcTemplate mainJdbcTemplate() {
+        return new JdbcTemplate(mainDataSource());
+    }
+}
